5cc9fb42f4a3856dc8c756bc0dd84a3be200ccad
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-10-31 16:31:54 +0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let c = celsius_to_fahrenheit(25.0);
+    let f = fahrenheit_to_celsius(18.0);
+    println!("{}", c);
+    println!("{}", f);
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    c * 1.8 + 32.0
+}
+
+fn fahrenheit_to_celsius(f: f64) -> f64 {
+    (f - 32.0) / 1.8
 }
